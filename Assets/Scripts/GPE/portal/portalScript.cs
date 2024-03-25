a32ff5ac8ae9f8717cc8c2c2558307381c5a692e
TortueFou <mathys.hymon@gmail.com>
2024-03-26 00:01:56 +0100

    [fix] portal

--- Assets/Scripts/GPE/portal/portalScript.cs
+++ Assets/Scripts/GPE/portal/portalScript.cs
@@ -6,23 +6,23 @@ public class portalScript : MonoBehaviour
     [SerializeField] private portalScript otherPortal;
     [SerializeField] private float DistanceToRender;
 
-    private GameObject cam;
-    private GameObject otherCam;
+    private Camera cam;
+    private Camera otherCam;
 
     private RenderTexture tempTexture;
 
     private bool playerOverlapping;
 
 
-    public GameObject GetCam()
+    public Camera GetCam()
     {
         return cam;
     }
 
     private void Awake()
     {
-        cam = transform.GetChild(0).gameObject;
-        cam.GetComponent<Camera>().fieldOfView = Camera.main.fieldOfView;
+        cam = transform.GetChild(0).gameObject.GetComponent<Camera>();
+        cam.fieldOfView = Camera.main.fieldOfView;
     }
     void Start()
     {
@@ -35,15 +35,17 @@ public class portalScript : MonoBehaviour
         if (Vector3.Distance(gameObject.transform.position, PlayerMovement.instance.transform.position) < DistanceToRender && targetManager.instance.VisibleFromCamera(transform.GetChild(1).gameObject))
         {
             Vector3 cameraOffset = Camera.main.transform.position - transform.position;
-            otherCam.GetComponent<Camera>().targetTexture = tempTexture;
-            otherCam.transform.localPosition = cameraOffset;
-            otherCam.transform.localRotation = Quaternion.LookRotation(MouseLook.instance.gameObject.transform.forward, Vector3.up);
+            otherCam.targetTexture = tempTexture;
+            otherCam.gameObject.transform.localPosition = cameraOffset;
+            otherCam.gameObject.transform.localRotation = Quaternion.LookRotation(MouseLook.instance.gameObject.transform.forward, Vector3.up);
             transform.GetChild(1).gameObject.GetComponent<MeshRenderer>().material.mainTexture = tempTexture;
+            PortalOffset(Camera.main);
+            otherPortal.PortalOffset(otherCam);
         }
         else
         {
-            otherCam.transform.localRotation = Quaternion.identity;
-            otherCam.transform.localPosition = Vector3.zero;
+            otherCam.gameObject.transform.localRotation = Quaternion.identity;
+            otherCam.gameObject.transform.localPosition = Vector3.zero;
         }
        
         //if (playerOverlapping && Vector3.Dot(-PlayerMovement.instance.gameObject.GetComponent<Rigidbody>().velocity.normalized, transform.forward.normalized) > 0)
@@ -58,6 +60,18 @@ public class portalScript : MonoBehaviour
         //    PlayerMovement.instance.transform.position = otherPortal.transform.position + positionOffset;
 
         //}
+    }
+
+    public void PortalOffset(Camera _cam)
+    {
+        float halfHeight = _cam.nearClipPlane * Mathf.Tan(_cam.fieldOfView * 0.5f * Mathf.Deg2Rad);
+        float halfWidth = halfHeight * _cam.aspect;
+        float dstToNearClipPlaneCorner = new Vector3(halfWidth, halfHeight, _cam.nearClipPlane).magnitude;
+
+        Transform screenT = transform.GetChild(1).transform;
+        bool camFacingSameDirAsPortal = Vector3.Dot(transform.forward, transform.position - _cam.transform.position) > 0;
+        screenT.localScale = new Vector3(screenT.localScale.x, screenT.localScale.y, dstToNearClipPlaneCorner);
+        screenT.localPosition = Vector3.forward * dstToNearClipPlaneCorner * ((camFacingSameDirAsPortal) ? 0.5f : -0.5f);
     }
 
 
@@ -78,11 +92,15 @@ public class portalScript : MonoBehaviour
             float rotationDiff = -Quaternion.Angle(transform.rotation, otherPortal.transform.rotation);
             rotationDiff += 180;
             PlayerMovement.instance.transform.Rotate(Vector3.up, rotationDiff);
-            PlayerMovement.instance.gameObject.GetComponent<Rigidbody>().velocity = Vector3.Scale(PlayerMovement.instance.gameObject.GetComponent<Rigidbody>().velocity, otherPortal.transform.forward);
+
+            // Calculer la nouvelle direction de la vitesse
+            Vector3 newVelocityDirection = Quaternion.AngleAxis(rotationDiff, Vector3.up) * PlayerMovement.instance.gameObject.GetComponent<Rigidbody>().velocity.normalized;
+
+            // Ajouter la nouvelle vitesse à la vitesse actuelle du joueur
+            PlayerMovement.instance.gameObject.GetComponent<Rigidbody>().velocity = newVelocityDirection * PlayerMovement.instance.gameObject.GetComponent<Rigidbody>().velocity.magnitude;
 
             Vector3 positionOffset = Quaternion.Euler(0f, rotationDiff, 0f) * offset;
             PlayerMovement.instance.transform.position = otherPortal.transform.position + positionOffset;
-
         }
     }
 
@@ -93,6 +111,7 @@ public class portalScript : MonoBehaviour
             playerOverlapping = false;
         }
     }
+
 
     private void OnDrawGizmos()
     {
